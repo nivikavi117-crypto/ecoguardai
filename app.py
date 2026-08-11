a23280743529741264a1676cf06d8adb434d98a8
nivikavi117-crypto <nivikavi117@gmail.com>
2026-08-11 12:10:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ st.sidebar.markdown("### Live Telemetry Control")
 input_temp = st.sidebar.slider("Temperature (°C)", 15, 50, 42)
 input_hum = st.sidebar.slider("Humidity (%)", 5, 95, 12)
 input_wind = st.sidebar.slider("Wind Speed (km/h)", 0, 60, 35)
-import requests
+    import requests
     try:
         geo_data = requests.get('https://ipapi.co', timeout=3).json()
         base_lat, base_lon = geo_data.get('latitude', 11.0181), geo_data.get('longitude', 76.9737)
