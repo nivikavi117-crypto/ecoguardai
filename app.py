9b3fbd5a194cb0bd0872dc8acf27fdcd33658bba
nivikavi117-crypto <nivikavi117@gmail.com>
2026-08-12 20:49:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -481,7 +481,19 @@ fire_score, fire_risk = calculate_fire_risk(
 # ============================================================
 # SIDEBAR
 # ===========================================================
-    st.markdown("---")
+   st.markdown(
+    "### Select Interface View"
+)
+
+interface = st.radio(
+    "Interface",
+    [
+        "🏠 Citizen Mobile App",
+        "🛡️ Department Admin Dashboard"
+    ],
+    label_visibility="collapsed"
+)
+st.markdown("---")
 
     st.markdown(
         "### 📍 Current Location"
